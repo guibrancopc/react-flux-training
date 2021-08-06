73ce62900f1ed7f9603f0e932bd0759eee4ce01f
Guilherme Carvalho <guibrancopc@gmail.com>
2021-08-06 19:53:27 -0300

    chore: module 5 done

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import HomePage from './HomePage';
+import AboutPage from './AboutPage';
+import Header from './common/Header';
+
+function getPage() {
+  const route = window.location.pathname;
+  if (route === '/about') {
+    return <AboutPage />;
+  }
+  return <HomePage />;
+}
+
+function App() {
+  return (
+    <div className="container-fluid">
+      <Header />
+      { getPage() }
+    </div>
+  )
+}
+
+export default App;
