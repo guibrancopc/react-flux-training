f1ca77248ed7e2fb20cb98a5750d80753250d459
Guilherme Carvalho <guibrancopc@gmail.com>
2021-08-09 18:21:32 -0300

    feat: add courses page

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import HomePage from './HomePage';
 import AboutPage from './AboutPage';
 import Header from './common/Header';
+import Courses from './CoursesPage';
 
 function getPage() {
   const route = window.location.pathname;
   if (route === '/about') {
     return <AboutPage />;
+  }
+  if (route === '/courses') {
+    return <Courses />;
   }
   return <HomePage />;
 }
